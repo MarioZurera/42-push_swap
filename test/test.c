80ffb65520223513d7739847fc2e2e8160803d34
mzurera- <mzurera-@student.42malaga.com>
2024-08-22 17:21:32 +0200

    test: added test main

---
+++ test/test.c
@@ -0,0 +1,22 @@
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzurera- <mzurera-@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 01:27:32 by mzurera-          #+#    #+#             */
+/*   Updated: 2024/08/02 23:43:05 by mzurera-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "unity.h"
+#include "push_swap_testing.h"
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+int main(int argc, char **argv) {
+	(void)argc;
+	(void)argv;
+	UNITY_BEGIN();
+	RUN_TEST(test_Addition);
+	return (UNITY_END());
+}
