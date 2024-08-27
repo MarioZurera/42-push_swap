18f84aede1f16603583a1d8f1dcc28d5b48afbec
mzurera- <mzurera-@student.42malaga.com>
2024-08-27 18:08:46 +0200

    feat: sort_5 schema

--- src/sort5.c
+++ src/sort5.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort.c                                             :+:      :+:    :+:   */
+/*   sort5.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mzurera- <mzurera-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,29 +12,7 @@
 
 #include "push_swap.h"
 
-void	sort_3(t_stack *stack_a, t_stack *stack_b)
+void	sort_5(t_stack *stack_a, t_stack *stack_b)
 {
-	sa(stack_a, stack_b, 1);
-	if (!is_ordered(stack_a))
-		sort_partial_ordered_stack_a(stack_a);
-}
 
-void	sort(t_stack *stack_a, t_stack *stack_b)
-{
-	if (stack_a == NULL || stack_b == NULL)
-		return ;
-	if (is_partially_ordered(stack_a))
-	{
-		sort_partial_ordered_stack_a(stack_a);
-		return ;
-	}
-	if (is_inversally_ordered(stack_a))
-	{
-		sort_inversally_ordered_stack_a(stack_a, stack_b);
-		return ;
-	}
-	if (stack_a->len <= 3)
-		sort_3(stack_a, stack_b);
-	//if (stack_a->len <= 5)
-	//	sort_5(stack_a, stack_b);
 }
