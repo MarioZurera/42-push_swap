c7cd24bae29913095c496364d2f32298b99f54bf
mzurera- <mzurera-@student.42malaga.com>
2024-09-08 19:29:33 +0200

    refactor: abstract logic from rra/rrb

--- src/movements/movement_reverse.c
+++ src/movements/movement_reverse.c
@@ -14,70 +14,31 @@
 
 int	rra(t_stack *stack_a, t_stack *stack_b, int print)
 {
-	int				n;
-	unsigned int	i;
-
 	(void) stack_b;
 	if (stack_a->len < 2)
 		return (0);
-	i = stack_a->len - 1;
-	n = stack_a->list[i];
-	while (i > 0)
-	{
-		stack_a->list[i] = stack_a->list[i - 1];
-		--i;
-	}
-	stack_a->list[i] = n;
-	if (print)
+	stack_a->push(stack_a, stack_a->pop_back(stack_a));
+	if (print != 0)
 		ft_printf("rra\n");
 	return (1);
 }
 
 int	rrb(t_stack *stack_a, t_stack *stack_b, int print)
 {
-	int				n;
-	unsigned int	i;
-
 	(void) stack_a;
-	if (stack_b->len < 2)
-		return (0);
-	i = stack_b->len - 1;
-	n = stack_b->list[i];
-	while (i > 0)
-	{
-		stack_b->list[i] = stack_b->list[i - 1];
-		--i;
-	}
-	stack_b->list[i] = n;
-	if (print)
+	stack_b->push(stack_b, stack_b->pop_back(stack_b));
+	if (print != 0)
 		ft_printf("rrb\n");
 	return (1);
 }
 
 int	rrr(t_stack *stack_a, t_stack *stack_b, int print)
 {
-	int				n;
-	unsigned int	i;
-
 	if (stack_a->len < 2 || stack_b->len < 2)
 		return (0);
-	i = stack_a->len - 1;
-	n = stack_a->list[i];
-	while (i > 0)
-	{
-		stack_a->list[i] = stack_a->list[i - 1];
-		--i;
-	}
-	stack_a->list[i] = n;
-	n = stack_b->list[stack_b->len - 1];
-	i = stack_b->len - 1;
-	while (i > 0)
-	{
-		stack_b->list[i] = stack_b->list[i - 1];
-		--i;
-	}
-	stack_b->list[i] = n;
-	if (print)
+	rra(stack_a, stack_b, 0);
+	rrb(stack_a, stack_b, 0);
+	if (print != 0)
 		ft_printf("rrr\n");
 	return (1);
 }
