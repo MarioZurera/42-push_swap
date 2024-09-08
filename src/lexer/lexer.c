395e806aa8a0bb6f96f6f504f6aeefda146bb96f
mzurera- <mzurera-@student.42malaga.com>
2024-09-08 17:37:12 +0200

    fix: memory leak

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -16,7 +16,7 @@ static t_str_array *get_list(int argc, char **argv)
 {
 	if (argc == 2)
 		return (string_array_from(ft_split(argv[1], ' ')));
-	return (string_array_from(argv + 1));
+	return (string_array_from_const(argv + 1));
 }
 
 static t_stack	*tokenize_list(t_str_array *list)
